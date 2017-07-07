d70a2c3927f4cd8444c9f3eb2c7896d989963d4b
Saxon <saxon.supple@gmail.com>
2017-07-07 15:48:52 +0100

    Update cpu.h

--- include/cpu.h
+++ include/cpu.h
@@ -1,68 +1,35 @@
 #ifndef CPU_H
 #define CPU_H
 
-#define AX(cpu) (cpu->registers->AX)
-#define BX(cpu) (cpu->registers->BX)
-#define CX(cpu) (cpu->registers->CX)
-#define DX(cpu) (cpu->registers->DX)
-#define AL(cpu) (cpu->registers->AX & 0x00FF)
-#define BL(cpu) (cpu->registers->BX & 0x00FF)
-#define CL(cpu) (cpu->registers->CX & 0x00FF)
-#define DL(cpu) (cpu->registers->DX & 0x00FF)
-#define AH(cpu) ((cpu->registers->AX & 0xFF00) >> 8)
-#define BH(cpu) ((cpu->registers->BX & 0xFF00) >> 8)
-#define CH(cpu) ((cpu->registers->CX & 0xFF00) >> 8)
-#define DH(cpu) ((cpu->registers->DX & 0xFF00) >> 8)
-#define SI(cpu) (cpu->registers->SI)
-#define DI(cpu) (cpu->registers->DI)
-#define BP(cpu) (cpu->registers->BP)
-#define SP(cpu) (cpu->registers->SP)
-#define CS(cpu) (cpu->registers->CS)
-#define DS(cpu) (cpu->registers->DS)
-#define SS(cpu) (cpu->registers->SS)
-#define ES(cpu) (cpu->registers->ES)
-#define IP(cpu) (cpu->registers->IP)
-#define EFLAGS(cpu) (cpu->registers->EFLAGS)
+#define AX(cpu) (cpu->registers[0])
+#define BX(cpu) (cpu->registers[3])
+#define CX(cpu) (cpu->registers->[1])
+#define DX(cpu) (cpu->registers->[2])
+#define SI(cpu) (cpu->registers[6])
+#define DI(cpu) (cpu->registers[7])
+#define BP(cpu) (cpu->registers[5])
+#define SP(cpu) (cpu->registers[4])
+#define CS(cpu) (cpu->CS)
+#define DS(cpu) (cpu->DS)
+#define SS(cpu) (cpu->SS)
+#define ES(cpu) (cpu->ES)
+#define IP(cpu) (cpu->IP)
+#define EFLAGS(cpu) (cpu->EFLAGS)
 
-typedef struct REGS
-{
-	//General purpose
-	//Acculumator (arithmetic and logic operations)
-	unsigned short AX;
-	//Memory pointer into DS
-	unsigned short BX;
-	//Counter register used in loops
-	unsigned short CX;
-	//Division and multiplication
-	unsigned short DX;
-
-	//Index registers for string ops
-	unsigned short SI;
-	unsigned short DI;
-
-	//Stack base pointer
-	unsigned short BP;
-	//Stack pointer
-	unsigned short SP;
-
-	//Code segment
-	unsigned short CS;
-	//Data segment
-	unsigned short DS;
-	//Stack segment
-	unsigned short SS;
-	//Extra segment
-	unsigned short ES;
-	//Instruction pointer
-	unsigned short IP;
-
-	//Flags
-	unsigned short EFLAGS;
-} REGS;
+#define REG(a) ((a & 0x1C) >> 3)
+#define RM(a) (a & 0x7)
 
 typedef struct CPU
 {
-	REGS *registers;
+	unsigned short registers[8];
+
+	unsigned short CS;
+	unsigned short DS;
+	unsigned short SS;
+	unsigned short ES;
+	unsigned short IP;
+	unsigned short EFLAGS;
+
 	//It's pipelined
 	unsigned char opcodes[6];
 	unsigned char opcode_index;
